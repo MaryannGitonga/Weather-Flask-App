2ed707d7f04158d1df9b69a803333522b61977f3
Maryann Gitonga <34104277+MaryannGitonga@users.noreply.github.com>
2020-09-10 14:09:14 +0300

    Add Flask and create home page route

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
+from flask import Flask, render_template, request
 import requests
 import configparser
+
+app = Flask(__name__)
+app.debug = True
+
+@app.route('/weather_dashboard')
+def weather_dashboard():
+    return render_template('index.html')
 
 def get_api_key():
     config = configparser.ConfigParser()
@@ -10,5 +18,6 @@ def get_weather_results(city_name, api_key):
     api_url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}".format(city_name, api_key)
     r = requests.get(api_url)
     return r.json()
-    
-print(get_weather_results("Nairobi", get_api_key())
+
+if __name__ == '__main__':
+    app.run()
