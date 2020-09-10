57d89a1b3aee5cf37fdc0a44fa7006e6af2423b9
Maryann Gitonga <34104277+MaryannGitonga@users.noreply.github.com>
2020-09-10 14:02:30 +0300

    Connect to the API and print out results to verify

---
+++ app.py
@@ -0,0 +1,11 @@
+def get_api_key():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    return config['openweathermap']['api']
+
+def get_weather_results(city_name, api_key):
+    api_url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}".format(city_name, api_key)
+    r = requests.get(api_url)
+    return r.json()
+    
+print(get_weather_results("Nairobi", get_api_key())
